67821ebd2492253e504ed728e7b39c18172d72db
Ale-zand <ale_zand@hotmail.com>
2023-05-09 14:39:36 -0300

    static, findById, router. Implementando o CRUD com o mongoDB e mongoose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 db.on("erro", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => {
@@ -11,46 +12,48 @@ const app = express();
 
 app.use(express.json())
 
+routes(app);
+
 // const livros = [
 //     {id: 1, "titulo": "O Senhor dos Anéis"},
 //     {id: 2, "titulo": "O Hobbit"}
 // ]
-
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node');
-})
-
-app.get('/livros', (req, res) => {
-    livros.find((err, livros) => {
-        res.status(200).json(livros);
-    })
-})
-
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-})
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro foi cadastrado com sucesso.');
-})
-
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-})
-
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params;          // desestruturação
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} removido com sucesso!`);
-})
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id);
-}
+// 
+// app.get('/', (req, res) => {
+//     res.status(200).send('Curso de Node');
+// })
+// 
+// app.get('/livros', (req, res) => {
+//     livros.find((err, livros) => {
+//         res.status(200).json(livros);
+//     })
+// })
+// 
+// app.get('/livros/:id', (req, res) => {
+//     let index = buscaLivro(req.params.id);
+//     res.json(livros[index]);
+// })
+// 
+// app.post('/livros', (req, res) => {
+//     livros.push(req.body);
+//     res.status(201).send('Livro foi cadastrado com sucesso.');
+// })
+// 
+// app.put('/livros/:id', (req, res) => {
+//     let index = buscaLivro(req.params.id);
+//     livros[index].titulo = req.body.titulo;
+//     res.json(livros);
+// })
+// 
+// app.delete('/livros/:id', (req, res) => {
+//     let {id} = req.params;          // desestruturação
+//     let index = buscaLivro(id);
+//     livros.splice(index, 1);
+//     res.send(`Livro ${id} removido com sucesso!`);
+// })
+// 
+// function buscaLivro(id) {
+//     return livros.findIndex(livro => livro.id == id);
+// }
 
 export default app
